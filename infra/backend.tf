b7637fd341dfa06266866da60bef65c6d9c497db
Karan Kapoor <93karankapoor@gmail.com>
2025-09-13 17:13:01 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   required_version = ">= 1.5.0"
   backend "s3" {
-    bucket         = "my-company-terraform-state-prod-v1"
-    key            = "prod/infra/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
+    bucket = "my-company-terraform-state-prod-v1"
+    key    = "prod/infra/terraform.tfstate"
+    region = "ap-south-1"
+    # dynamodb_table = "terraform-state-locking"
+    encrypt = true
   }
 }
